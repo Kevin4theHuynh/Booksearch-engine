a89f05a320f5b68d8419127b6ae337ef6dabaf43
Kevin4theHuynh <85657180+Kevin4theHuynh@users.noreply.github.com>
2021-11-30 20:32:49 -0800

    index.js

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -3,22 +3,31 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 const { ApolloServer } = require("apollo-server-express")
-const { autMiddleware } = require("./utils/auth")
+const { autMiddleware } = require("./utils/auth");
+const { resolveGraphqlOptions } = require('apollo-server-core');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: autMiddleware,
+})
 
+server.applyMiddleware({ app })
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"))
+})
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
-app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
