b9a08a54dd8eff4520121216fb1172a751860d8e
Kevin4theHuynh <85657180+Kevin4theHuynh@users.noreply.github.com>
2021-11-30 20:49:49 -0800

    Added more fields

--- Develop/server/schema/typeDefs.js
+++ Develop/server/schema/typeDefs.js
@@ -11,6 +11,35 @@ type User {
 
 type Auth {
     token: ID!
+    user: User
+}
+
+type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    image: String
+    link: String
+}
+
+input InputBook {
+    bookId: String
+    authors: [String]
+    title: String
+    description: String
+    image: String
+    link: String
+}
+
+type Query {
+    me: User
+}
+
+type Mutation {
+    login(email: String, password: String!): Auth
+    saveBook(newBook: InputBook!): User
+    addUser(username: String!, email: String!, password:String!): Auth
+    removeBook(bookId: ID!): User
 }
 
 `
