72a62dba4029bbb392b879fa0d09a09ff8675a7b
Kevin4theHuynh <85657180+Kevin4theHuynh@users.noreply.github.com>
2021-11-30 21:18:10 -0800

    mutations

--- Develop/server/schema/resolvers.js
+++ Develop/server/schema/resolvers.js
@@ -15,7 +15,24 @@ const resolvers = {
 
     Mutations: {
         addUser: async (parent, args) => {
-            const user = await 
-        }
+            const user = await User.create(args)
+            const token = signToken(user);
+            return {token, user};
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email })
 
+            if (!user) {
+                throw new AuthenticationError('No user found')
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+              }
+
+            const token = signToken(user);
+            return {token , user};
+        }
     }
