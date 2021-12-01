c316bf5595f27f0705ffa964086737680f92fb3c
Kevin4theHuynh <85657180+Kevin4theHuynh@users.noreply.github.com>
2021-11-30 18:01:49 -0800

    Added mutations js

---
+++ Develop/client/src/utils/mutations.js
@@ -0,0 +1,19 @@
+import { gql } from '@aapollo/client'
+
+export const LOGIN_USER = gql`
+    mutation login(
+        $email: String!
+        $password: String!
+) {
+    login(
+        email: $$email
+        password: $$password
+    ) {
+        token
+        user {
+            _id
+            username
+        }
+    }
+}
+`
